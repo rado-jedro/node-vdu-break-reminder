06938b7e98390e0f22d5de118195ec0546ee0027
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-08 18:18:52 +0000

    Add conditions to display the elements

--- app/script.js
+++ app/script.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      status: 'off',
+      status: "off",
       time: 1200,
-      timer:null,
+      timer: null
     };
   }
 
   render() {
+    const { status } = this.state;
+
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
+        {status === "off" && (
+          <div>
+            <p>
+              According to optometrists in order to save your eyes, you should
+              follow the 20/20/20. It means you should to rest your eyes every
+              20 minutes for 20 seconds by looking more than 20 feet away.
+            </p>
+            <p>
+              This app will help you track your time and inform you when it's
+              time to rest.
+            </p>
+          </div>
+        )}
+        {status === "work" && <img src="./images/work.png" />}
+        {status === "rest" && <img src="./images/rest.png" />}
+        {status !== "off" && <div className="timer">18:23</div>}
+        {status === "off" && <button className="btn">Start</button>}
+        {status !== "off" && <button className="btn">Stop</button>}
         <button className="btn btn-close">X</button>
       </div>
-    )
+    );
   }
-};
+}
 
-render(<App />, document.querySelector('#app'));
+render(<App />, document.querySelector("#app"));
